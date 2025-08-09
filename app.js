975e8639d6cd65c9079286d6590608d3ce9c1800
elministroisrael <elministroisrael@gmail.com>
2025-08-09 12:44:29 -0400

    agregar validacion

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ let amigos=[];
  function agregarAmigo() {
       const input = document.getElementById('nombre');
       const nombre = input.value.trim();
+
+       // Validación
+      if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
  }
+
+        
+}
